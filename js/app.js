0a200375292b3a6935a713f202227f53d228a5a6
isobelmorales <isofmorales@gmail.com>
2023-01-20 10:04:20 -0500

    added button functionality

--- js/app.js
+++ js/app.js
@@ -22,41 +22,41 @@ const daemonInfo = document.getElementById('daemon-info')
 const laenorInfo = document.getElementById('laenor-info')
 const laenaInfo = document.getElementById('laena-info')
 
-// Rhaenyra Targaryen
-"Rhaenyra is the daughter and only surviving child of King Viserys I and Aemma Arryn. She rides the dragon Syrax."
-"Alive"
-
-// Alicent Hightower
-"Alicent Hightower is the daughter of Ser Otto Hightower, the Hand of the King. She has a number of brothers, the eldest being Ser Gwayne Hightower. From an early age, she has lived in the Red Keep and grown up among the royal family."
-"Alive"
-
-// King Viserys
-"King Viserys I Targaryen,[N 2] remembered as Viserys the Peaceful, was the fifth king of the Targaryen dynasty to rule the Seven Kingdoms. Viserys I was the grandson of Jaehaerys I Targaryen, and father of Rhaenyra. He briefly rode Balerion the Black Dread in his youth before the dragon's death of old age."
-"Deceased"
-
-// Daemon Targaryen
-"Prince Daemon Targaryen is a prince of the Targaryen dynasty, and the younger brother of King Viserys I Targaryen. He is the uncle of Queen Rhaenyra Targaryen, and later becomes her second husband and king consort.He is a peerless warrior, and also a dragonrider: his mount is Caraxes, the Blood Wyrm."
-"Alive"
-
-// Laenor Velaryon
-"Ser Laenor Velaryon is the son of Corlys Velaryon and Rhaenys Targaryen, and the presumed deceased former heir to Driftmark. He once rode the dragon Seasmoke."
-"Alive"
-
-// Laena Velaryon
-"Lady Laena Velaryon was the daughter of Corlys Velaryon and Rhaenys Targaryen and the sister of Laenor Velaryon. She was the rider of the dragon Vhagar."
-"Deceased"
-
 //// EVENT LISTENERS ////
 
-rhaenyraBio.addEventListener('click', )
-rhaenyraStatus.addEventListener('click', )
-alicentBio.addEventListener('click', )
-alicentStatus.addEventListener('click', )
-viserysBio.addEventListener('click', )
-viserysStatus.addEventListener('click', )
-daemonBio.addEventListener('click', )
-daemonStatus.addEventListener('click', )
-laenorBio.addEventListener('click', )
-laenorStatus.addEventListener('click', )
-laenaBio.addEventListener('click', )
-laenaStatus.addEventListener('click', )
+rhaenyraBio.addEventListener('click', () => {
+    rhaenyraInfo.innerText = "Rhaenyra is the daughter and only surviving child of King Viserys I and Aemma Arryn. She rides the dragon Syrax."
+})
+rhaenyraStatus.addEventListener('click', () => {
+    rhaenyraInfo.innerText = "Alive"
+})
+alicentBio.addEventListener('click', () => {
+    alicentInfo.innerText = "Rhaenyra is the daughter and only surviving child of King Viserys I and Aemma Arryn. She rides the dragon Syrax."
+})
+alicentStatus.addEventListener('click', () => {
+    alicentInfo.innerText = "Alive"
+})
+viserysBio.addEventListener('click', () => {
+    viserysInfo.innerText = "King Viserys I Targaryen, remembered as Viserys the Peaceful, was the fifth king of the Targaryen dynasty to rule the Seven Kingdoms. Viserys I was the grandson of Jaehaerys I Targaryen, and father of Rhaenyra. He briefly rode Balerion the Black Dread in his youth before the dragon's death of old age."
+})
+viserysStatus.addEventListener('click', () => {
+    viserysInfo.innerText = "Deceased"
+})
+daemonBio.addEventListener('click', () => {
+    daemonInfo.innerText = "Prince Daemon Targaryen is a prince of the Targaryen dynasty, and the younger brother of King Viserys I Targaryen. He is the uncle of Queen Rhaenyra Targaryen, and later becomes her second husband and king consort.He is a peerless warrior, and also a dragonrider: his mount is Caraxes, the Blood Wyrm."
+})
+daemonStatus.addEventListener('click', () => {
+    daemonInfo.innerText = "Alive"
+})
+laenorBio.addEventListener('click', () => {
+    laenorInfo.innerText = "Ser Laenor Velaryon is the son of Corlys Velaryon and Rhaenys Targaryen, and the presumed deceased former heir to Driftmark. He once rode the dragon Seasmoke."
+})
+laenorStatus.addEventListener('click', () => {
+    laenorInfo.innerText = "Alive"
+})
+laenaBio.addEventListener('click', () => {
+    laenaInfo.innerText = "Lady Laena Velaryon was the daughter of Corlys Velaryon and Rhaenys Targaryen and the sister of Laenor Velaryon. She was the rider of the dragon Vhagar."
+})
+laenaStatus.addEventListener('click', () => {
+    laenaInfo.innerText = "Deceased"
+})

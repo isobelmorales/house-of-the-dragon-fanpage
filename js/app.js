344d9bcc4f6bdffe06ea476b0d01da578f2e2f3c
isobelmorales <isofmorales@gmail.com>
2023-01-20 09:56:11 -0500

    created layout and started adding js

---
+++ js/app.js
@@ -0,0 +1,62 @@
+//// DOM ELEMENTS ////
+
+// buttons
+const rhaenyraBio = document.getElementById('rhaenyra-bio')
+const rhaenyraStatus = document.getElementById('rhaenyra-status')
+const alicentBio = document.getElementById('alicent-bio')
+const alicentStatus = document.getElementById('alicent-status')
+const viserysBio = document.getElementById('viserys-bio')
+const viserysStatus = document.getElementById('viserys-status')
+const daemonBio = document.getElementById('daemon-bio')
+const daemonStatus = document.getElementById('daemon-status')
+const laenorBio = document.getElementById('laenor-bio')
+const laenorStatus = document.getElementById('laenor-status')
+const laenaBio = document.getElementById('laena-bio')
+const laenaStatus = document.getElementById('laena-status')
+
+// displays
+const rhaenyraInfo = document.getElementById('rhaenyra-info')
+const alicentInfo = document.getElementById('alicent-info')
+const viserysInfo = document.getElementById('viserys-info')
+const daemonInfo = document.getElementById('daemon-info')
+const laenorInfo = document.getElementById('laenor-info')
+const laenaInfo = document.getElementById('laena-info')
+
+// Rhaenyra Targaryen
+"Rhaenyra is the daughter and only surviving child of King Viserys I and Aemma Arryn. She rides the dragon Syrax."
+"Alive"
+
+// Alicent Hightower
+"Alicent Hightower is the daughter of Ser Otto Hightower, the Hand of the King. She has a number of brothers, the eldest being Ser Gwayne Hightower. From an early age, she has lived in the Red Keep and grown up among the royal family."
+"Alive"
+
+// King Viserys
+"King Viserys I Targaryen,[N 2] remembered as Viserys the Peaceful, was the fifth king of the Targaryen dynasty to rule the Seven Kingdoms. Viserys I was the grandson of Jaehaerys I Targaryen, and father of Rhaenyra. He briefly rode Balerion the Black Dread in his youth before the dragon's death of old age."
+"Deceased"
+
+// Daemon Targaryen
+"Prince Daemon Targaryen is a prince of the Targaryen dynasty, and the younger brother of King Viserys I Targaryen. He is the uncle of Queen Rhaenyra Targaryen, and later becomes her second husband and king consort.He is a peerless warrior, and also a dragonrider: his mount is Caraxes, the Blood Wyrm."
+"Alive"
+
+// Laenor Velaryon
+"Ser Laenor Velaryon is the son of Corlys Velaryon and Rhaenys Targaryen, and the presumed deceased former heir to Driftmark. He once rode the dragon Seasmoke."
+"Alive"
+
+// Laena Velaryon
+"Lady Laena Velaryon was the daughter of Corlys Velaryon and Rhaenys Targaryen and the sister of Laenor Velaryon. She was the rider of the dragon Vhagar."
+"Deceased"
+
+//// EVENT LISTENERS ////
+
+rhaenyraBio.addEventListener('click', )
+rhaenyraStatus.addEventListener('click', )
+alicentBio.addEventListener('click', )
+alicentStatus.addEventListener('click', )
+viserysBio.addEventListener('click', )
+viserysStatus.addEventListener('click', )
+daemonBio.addEventListener('click', )
+daemonStatus.addEventListener('click', )
+laenorBio.addEventListener('click', )
+laenorStatus.addEventListener('click', )
+laenaBio.addEventListener('click', )
+laenaStatus.addEventListener('click', )
